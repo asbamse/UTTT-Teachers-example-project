8d1184baeb65d1209ccea8e1eb4b664749ecc745
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-03-06 17:06:44 +0100

    removed unused constructer

--- src/dk/easv/bll/bot/MonteCarloBot.java
+++ src/dk/easv/bll/bot/MonteCarloBot.java
@@ -22,13 +22,10 @@ public class MonteCarloBot implements IBot {
     
     private List<Integer[]> results; //int[0] is total tries int[1] is score 1 for each win 0 for each draw and -1 for each lose
     private List<IMove> myMoves;
-    private final static int MAX_TIME_FOR_SEARCHING;
+    private final static int MAX_TIME_FOR_SEARCHING = 3000;
     private IGameState currentState;
     private int searches = 0;
     
-    public MonteCarloBot() {
-        MAX_TIME_FOR_SEARCHING = 3000;//MaxSearchTimeInMilliseconds
-    }
     
     @Override
     public IMove doMove(IGameState state) {
